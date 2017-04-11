77cc573b40ad87d56865f42bfdd28d1c1ca5fbcf
Morgan Ernest <morgan@weecology.org>
2017-04-11 14:00:19 -0400

    Add tests for rodent df column names

--- test/testthat/test-dataprocessing.R
+++ test/testthat/test-dataprocessing.R
@@ -12,4 +12,26 @@ test_that("argument repo returns list length 5", {
   expect_that(length(data), equals(5))
 })
 
+test_that("required column names in rodent df", {
+  data = loadData("repo")
+  rats = data[[1]]
+  rat_cols = colnames(rats)
+  expect_true('mo' %in% rat_cols)
+  expect_true('yr' %in% rat_cols)
+  expect_true('plot' %in% rat_cols)
+  expect_true('species' %in% rat_cols)
+  expect_true('period' %in% rat_cols)
+  expect_true('dy' %in% rat_cols)
+})
 
+test_that("required column names in rodent df", {
+  data = loadData("repo")
+  sp = data[[2]]
+  sp_cols = colnames(sp)
+  expect_true('species' %in% sp_cols)
+  expect_true('yr' %in% rat_cols)
+  expect_true('plot' %in% rat_cols)
+  expect_true('species' %in% rat_cols)
+  expect_true('period' %in% rat_cols)
+  expect_true('dy' %in% rat_cols)
+})
