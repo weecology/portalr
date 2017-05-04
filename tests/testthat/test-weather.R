9a0d43294b99e64d1d115914f75e9a4dbd3998fc
Glenda <glenda@weecology.org>
2017-05-03 21:03:54 -0600

    Fix path in weather tests

--- tests/testthat/test-weather.R
+++ tests/testthat/test-weather.R
@@ -1,27 +1,29 @@
 context("checks weather summary output")
 
+download_observations('.')
+
 test_that("'Daily' option returns 7 columns" , {
-  data = weather("Daily")
+  data = weather("Daily",".")
   expect_that(dim(data)[2], equals(7))
   expect_that(sum(colnames(data)==c("Year","Month","Day","MinTemp","MaxTemp","MeanTemp","Precipitation")), equals(7))
 })
 
 test_that("Monthly option returns 7 columns", {
-  data = weather("Monthly")
+  data = weather("Monthly",".")
   expect_that(dim(data)[2], equals(7))
   expect_that(sum(colnames(data)==c("Year","Month","MinTemp","MaxTemp","MeanTemp","Precipitation","NDVI")), equals(7))
 })
 
 
 test_that("Daily temperatures ok" , {
-  data = weather("Daily")
+  data = weather("Daily",".")
   expect_that(length(which((data$MinTemp<=data$MaxTemp)==FALSE)),equals(0))
   expect_that(length(which((data$MeanTemp<=data$MaxTemp)==FALSE)),equals(0))
   expect_that(length(which((data$MinTemp<=data$MeanTemp)==FALSE)),equals(0))
 })
 
 test_that("Monthly temperatures ok", {
-  data = weather("Monthly")
+  data = weather("Monthly",".")
   expect_that(length(which((data$MinTemp<=data$MaxTemp)==FALSE)),equals(0))
   expect_that(length(which((data$MeanTemp<=data$MaxTemp)==FALSE)),equals(0))
   expect_that(length(which((data$MinTemp<=data$MeanTemp)==FALSE)),equals(0))
