d14fe3690384f68dba48e3eda3a3764ebec410e0
Ethan White <ethan@weecology.org>
2017-07-22 10:22:01 -0400

    Update NAMESPACE and docs

---
+++ R/PortalDataSummaries-package.R
@@ -0,0 +1,12 @@
+#' @title Portal Data Summaries
+#' 
+#' @import dplyr
+#' @import tidyr
+#' @import RCurl
+#' @import lubridate
+#' @import zoo
+#' @import ggplot2
+#' @name PortalDataSummaries
+#' @docType package
+#' @keywords package
+NULL
