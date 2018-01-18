9f03aef607a21ebe9eb110f49ad8a5406d9866ec
Renata Diaz <diaz.renatam@gmail.com>
2018-01-18 11:19:13 -0500

    Abundance, biomass, energy tests
    Also moving test-getRodentData.R to tests/testthat - that seems like where it should go, although I'm not certain.

--- tests/testthat/test-getRodentData.R
+++ tests/testthat/test-getRodentData.R
@@ -38,7 +38,7 @@ test_that("abundance returns expected results", {
                         shape = "flat", time = "period", fillweight = F)
   test.abundance = filter(abundance.notfilled, period %in% 400:450)
   expect_true(sum(test.abundance$abundance, na.rm = T) == 10622)
-  test.abundance = filter(abundance, species == 'DM', abundance > 0)
+  test.abundance = filter(abundance.notfilled, species == 'DM', abundance > 0)
   expect_true(max(test.abundance$abundance, na.rm = T) == 17)
   expect_true(anyNA(test.abundance) == F)
   expect_true(nrow(test.abundance) == 4948)
@@ -47,18 +47,54 @@ test_that("abundance returns expected results", {
                                length = "all", unknowns = T, incomplete = T,
                                shape = "flat", time = "period", fillweight = T)
   expect_true(length(which(abundance.notfilled != abundance.filled)) == 0)
+
 })
 
 test_that("biomass returns expected results", {
-  biomass = biomass(path = 'repo', level = "Site", type = "Rodents",
+  biomass.filled = biomass(path = 'repo', level = "Plot", type = "Rodents",
                         length = "all", unknowns = T, incomplete = T,
                         shape = "flat", time = "period", fillweight = T)
-})
+  biomass.test = filter(biomass.filled, period %in% 400:450)
+  biomass.notfilled = biomass(path = 'repo', level = "Plot", type = "Rodents",
+                              length = "all", unknowns = T, incomplete = T,
+                              shape = "flat", time = "period", fillweight = F)
+
+  biomass.notfilledt = filter(biomass.notfilled, period %in% 400:450)
+  expect_true(nrow(biomass.test) == 26928)
+  expect_true(length(which(dim(biomass.test) != dim(biomass.notfilledt))) == 0)
+
+  expect_true(length(which(is.na(biomass.test$biomass))) == 1166)
+  expect_true(length(which(is.na(biomass.notfilledt$biomass))) == 1166)
+  expect_true(length(which(biomass.notfilledt$species != biomass.test$species)) == 0)
+
+  expect_true(which(biomass.notfilledt$biomass != biomass.test$biomass)[2] == 1131)
+  expect_true(as.numeric(biomass.notfilledt[1131, 'biomass']) == 15)
+  expect_true(floor(as.numeric(biomass.test[1131, 'biomass'])) == 24)
+
+  })
 
 test_that("energy returns expected results", {
-  energy = energy(path = 'repo', level = "Site", type = "Rodents",
+  energy.filled = energy(path = 'repo', level = "Plot", type = "Rodents",
                         length = "all", unknowns = T, incomplete = T,
                         shape = "flat", time = "period", fillweight = T)
-})
+
+  energy.test = filter(energy.filled, period %in% 400:450)
+  energy.notfilled = energy(path = 'repo', level = "Plot", type = "Rodents",
+                              length = "all", unknowns = T, incomplete = T,
+                              shape = "flat", time = "period", fillweight = F)
+
+  energy.notfilledt = filter(energy.notfilled, period %in% 400:450)
+  expect_true(nrow(energy.test) == 26928)
+  expect_true(length(which(dim(energy.test) != dim(energy.notfilledt))) == 0)
+
+  expect_true(length(which(is.na(energy.test$energy))) == 1166)
+  expect_true(length(which(is.na(energy.notfilledt$energy))) == 1166)
+  expect_true(length(which(energy.notfilledt$species != energy.test$species)) == 0)
+
+  expect_true(which(energy.notfilledt$energy != energy.test$energy)[2] == 1131)
+  expect_true(floor(as.numeric(energy.notfilledt[1131, 'energy'])) == 9)
+  expect_true(floor(as.numeric(energy.test[1131, 'energy'])) == 14)
+
+  })
 
 
