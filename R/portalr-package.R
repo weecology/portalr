dca2b927948c8a1b9cb295a219da188f6ffb14a2
Hao Ye <lhopitalified@gmail.com>
2018-01-20 20:12:22 -0500

    cleaning up tidyr dependency and simplifying vignette

--- R/portalr-package.R
+++ R/portalr-package.R
@@ -2,7 +2,6 @@
 #'
 #' @description This package is designed to be an interface to the Portal data, which resides online at \url{https://github.com/weecology/portalData}. Its contains a set of functions to download, clean, and summarize the data.
 #'
-#' @import tidyr
 #' @import ggplot2
 #' @name portalr
 #' @docType package
