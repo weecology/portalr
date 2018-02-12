cb0c7221dab266af1ff84e5f43f802bea2acf19b
Erica Christensen <erica.christensen@weecology.org>
2018-02-12 10:56:30 -0500

    fixed path in tests

--- tests/testthat/test-06-plants.R
+++ tests/testthat/test-06-plants.R
@@ -1,7 +1,7 @@
 context("Check plant data summaries")
 
 test_that("plant_abundance returns expected results", {
-  plantabundance.all = plant_abundance(path = '..', level = "Plot", type = "All",
+  plantabundance.all = plant_abundance(path = '.', level = "Plot", type = "All",
                                   length = "all", unknowns = T, correct_sp= F,
                                   shape = "flat")
   test.plantabundance = dplyr::filter(plantabundance.all, year %in% 1996:1998)
@@ -11,7 +11,7 @@ test_that("plant_abundance returns expected results", {
   expect_false(anyNA(test.plantabundance))
   expect_equal(nrow(test.plantabundance), 592)
 
-  plantabundance.fixnames = plant_abundance(path = '..', level = "Plot", type = "All",
+  plantabundance.fixnames = plant_abundance(path = '.', level = "Plot", type = "All",
                                             length = "all", unknowns = T, correct_sp= T,
                                             shape = "flat")
   expect_equal(sum(plantabundance.all$abundance,na.rm=T), sum(plantabundance.fixnames$abundance,na.rm=T))
