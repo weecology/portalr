032e74716f5b10779f96a19757e621a40435af8d
Glenda <gmyenni@users.noreply.github.com>
2018-03-01 11:12:45 -0700

    Use regional stations to fill gaps in weather data, closes #4 (#100)
    * Use regional stations to fill gaps in weather data, closes #4

--- R/Weather.R
+++ R/Weather.R
@@ -4,13 +4,14 @@
 #'
 #' @description Summarize hourly weather data to either daily or monthly level.
 #'
-#' @param level specify 'Monthly' or 'Daily'
+#' @param level specify 'monthly' or 'daily'
 #' @param fill specify if missing data should be filled, passed to \code{fill_missing_weather}
 #' @param path specify where to locate Portal data
 #'
 #' @export
 #'
-weather <- function(level, path = '~') {
+weather <- function(level = "daily", fill = FALSE, path = '~') {
+  level = tolower(level)
   weather_new=read.csv(FullPath('PortalData/Weather/Portal_weather.csv', path), na.strings=c(""), stringsAsFactors = FALSE)
   weather_old=read.csv(FullPath('PortalData/Weather/Portal_weather_19801989.csv', path), na.strings=c("-99"), stringsAsFactors = FALSE)
   NDVI=read.csv(FullPath('PortalData/NDVI/monthly_NDVI.csv', path), na.strings=c("-99"), stringsAsFactors = FALSE)
@@ -18,12 +19,15 @@ weather <- function(level, path = '~') {
   ###########Summarise by Day ----------------------
   days = weather_new %>%
     dplyr::group_by(year, month, day) %>%
-    dplyr::summarize(mintemp=min(airtemp),maxtemp=max(airtemp),meantemp=mean(airtemp),precipitation=sum(precipitation))
+    dplyr::summarize(mintemp=min(airtemp),maxtemp=max(airtemp),meantemp=mean(airtemp),precipitation=sum(precipitation),battv=min(battv,na.rm=TRUE))
 
-  weather=dplyr::bind_rows(weather_old[1:3442,],days) %>%
-    dplyr::select(year,month,day,mintemp,maxtemp,meantemp,precipitation)
+  weather=dplyr::bind_rows(weather_old[1:3442,],days)  %>%
+    dplyr::mutate(locally_measured = TRUE, battv = ifelse(is.infinite(battv),NA,battv), battery_low = ifelse(battv<11,TRUE,FALSE)) %>%
+    dplyr::select(year,month,day,mintemp,maxtemp,meantemp,precipitation,locally_measured,battery_low)
 
-  if (level=='Monthly') {
+    weather = fill_missing_weather(weather, fill, path)
+
+  if (level=='monthly') {
 
     ##########Summarise by Month -----------------
 
@@ -37,13 +41,65 @@ weather <- function(level, path = '~') {
 
     weather = weather %>%
       dplyr::group_by(year, month) %>%
-      dplyr::summarize(mintemp=min(mintemp,na.rm=T),maxtemp=max(maxtemp,na.rm=T),meantemp=mean(meantemp,na.rm=T),precipitation=sum(precipitation,na.rm=T)) %>%
-      dplyr::full_join(NDVI) %>%
+      dplyr::summarize(mintemp=min(mintemp,na.rm=T),maxtemp=max(maxtemp,na.rm=T),meantemp=mean(meantemp,na.rm=T),
+                       precipitation=sum(precipitation,na.rm=T), locally_measured=all(locally_measured),
+                       battery_low=all(battery_low,na.rm=TRUE)) %>%
+      dplyr::full_join(NDVI, by = c("year", "month")) %>%
       dplyr::arrange(year,month) %>%
-      dplyr::select(year, month, mintemp, maxtemp, meantemp, precipitation, ndvi) %>%
-      dplyr::mutate(ndvi = as.numeric(ndvi))
+      dplyr::select(year, month, mintemp, maxtemp, meantemp, precipitation, ndvi, locally_measured, battery_low) %>%
+      dplyr::mutate(ndvi = as.numeric(ndvi), battery_low=ifelse(year<2003,NA,battery_low))
   }
 
 
   return(weather)
+}
+
+
+#' @title Fill missing weather with regional data
+#'
+#' @description Use two weather stations in San Simon valley to fill in
+#' missing weather data in the daily time series
+#'
+#' @param daily a dataframe of daily weather data
+#' @param fill specify if missing data should be filled
+#' @param path specify where to locate regional data
+#'
+#' @export
+#'
+fill_missing_weather <- function(daily, fill,path) {
+
+  if(fill) {
+    portal4sw = read.csv(FullPath('PortalData/Weather/Portal4sw_regional_weather.csv', path), na.strings=c(""), header=T,
+                         colClasses=c("character", rep("integer",3), "character", "integer", rep("character",3), "Date"))
+    sansimon = read.csv(FullPath('PortalData/Weather/Sansimon_regional_weather.csv', path), na.strings=c(""), header=T,
+                        colClasses=c("character", rep("integer",3), "character", "integer", rep("character",3), "Date"))
+    region = dplyr::full_join(portal4sw,sansimon,by=c("date","year","month","day","element")) %>% dplyr::filter(date >= "1980-01-01")
+
+    precip = region %>% dplyr::filter(element == "PRCP") %>%
+      dplyr::mutate(precip = rowMeans(cbind(value.x/10,value.y/10),na.rm=T)) %>%
+      dplyr::select(year,month,day,precip)
+    tmin = region %>% dplyr::filter(element == "TMIN") %>%
+      dplyr::mutate(tmin = rowMeans(cbind(value.x/10,value.y/10),na.rm=T)) %>%
+      dplyr::select(year,month,day,tmin)
+    tmax = region %>% dplyr::filter(element == "TMAX") %>%
+      dplyr::mutate(tmax = rowMeans(cbind(value.x/10,value.y/10),na.rm=T)) %>%
+      dplyr::select(year,month,day,tmax)
+    tobs = region %>% dplyr::filter(element == "TOBS") %>%
+      dplyr::mutate(tobs = rowMeans(cbind(value.x/10,value.y/10),na.rm=T)) %>%
+      dplyr::select(year,month,day,tobs)
+
+    regionmeans = precip %>% dplyr::group_by(year,month,day) %>%
+      dplyr::full_join(tmin,by = c("year", "month", "day")) %>% dplyr::full_join(tmax,by = c("year", "month", "day")) %>%
+      dplyr::full_join(tobs,by = c("year", "month", "day")) %>% dplyr::arrange(year,month,day) %>% dplyr::distinct(.)
+
+    filled_data = dplyr::full_join(regionmeans,daily,by = c("year", "month", "day")) %>% dplyr::arrange(year,month,day) %>%
+      dplyr::mutate(locally_measured = ifelse(any(is.na(c(mintemp,maxtemp,meantemp,precipitation))),FALSE,TRUE),
+                    mintemp = ifelse(is.na(mintemp),tmin,mintemp), maxtemp = ifelse(is.na(maxtemp),tmax,maxtemp),
+                    meantemp = ifelse(is.na(meantemp),tobs,meantemp),
+                    precipitation = ifelse(is.na(precipitation),precip,precipitation)) %>%
+      dplyr::select(year,month,day,mintemp,maxtemp,meantemp,precipitation,locally_measured,battery_low)
+
+    daily = filled_data
+    }
+  return(daily)
 }
--- tests/testthat/test-04-weather.R
+++ tests/testthat/test-04-weather.R
@@ -1,30 +1,26 @@
 context("checks weather summary output")
 
-download_observations('.')
+daily_weather = weather("daily",path = ".")
+monthly_weather = weather("monthly",path = ".")
 
-test_that("'Daily' option returns 7 columns" , {
-  data = weather("Daily",path = ".")
-  expect_that(dim(data)[2], equals(7))
-  expect_that(sum(colnames(data)==c("year","month","day","mintemp","maxtemp","meantemp","precipitation")), equals(7))
+test_that("'Daily' option returns 9 columns" , {
+  expect_that(dim(daily_weather)[2], equals(9))
+  expect_that(sum(colnames(daily_weather)==c("year","month","day","mintemp","maxtemp","meantemp","precipitation","locally_measured","battery_low")), equals(9))
 })
-
-test_that("Monthly option returns 7 columns", {
-  data = weather("Monthly",path = ".")
-  expect_that(dim(data)[2], equals(7))
-  expect_that(sum(colnames(data)==c("year","month","mintemp","maxtemp","meantemp","precipitation","ndvi")), equals(7))
-})
-
 
 test_that("Daily temperatures ok" , {
-  data = weather("Daily",path = ".")
-  expect_that(length(which((data$mintemp<=data$maxtemp)==FALSE)),equals(0))
-  expect_that(length(which((data$meantemp<=data$maxtemp)==FALSE)),equals(0))
-  expect_that(length(which((data$mintemp<=data$meantemp)==FALSE)),equals(0))
+  expect_that(length(which((daily_weather$mintemp<=daily_weather$maxtemp)==FALSE)),equals(0))
+  expect_that(length(which((daily_weather$meantemp<=daily_weather$maxtemp)==FALSE)),equals(0))
+  expect_that(length(which((daily_weather$mintemp<=daily_weather$meantemp)==FALSE)),equals(0))
+})
+
+test_that("Monthly option returns 9 columns", {
+  expect_that(dim(monthly_weather)[2], equals(9))
+  expect_that(sum(colnames(monthly_weather)==c("year","month","mintemp","maxtemp","meantemp","precipitation","ndvi","locally_measured","battery_low")), equals(9))
 })
 
 test_that("Monthly temperatures ok", {
-  data = weather("Monthly",path = ".")
-  expect_that(length(which((data$mintemp<=data$maxtemp)==FALSE)),equals(0))
-  expect_that(length(which((data$meantemp<=data$maxtemp)==FALSE)),equals(0))
-  expect_that(length(which((data$mintemp<=data$meantemp)==FALSE)),equals(0))
+  expect_that(length(which((monthly_weather$mintemp<=monthly_weather$maxtemp)==FALSE)),equals(0))
+  expect_that(length(which((monthly_weather$meantemp<=monthly_weather$maxtemp)==FALSE)),equals(0))
+  expect_that(length(which((monthly_weather$mintemp<=monthly_weather$meantemp)==FALSE)),equals(0))
 })
