d98d802d0a1e92053e67ad07a789f55c1c808f38
Glenda Yenni <glenda@weecology.org>
2018-04-19 00:13:12 -0600

    Set min_plots default to 24, change incomplete argument to fill_incomplete

--- tests/testthat/test-02-data-processing.R
+++ tests/testthat/test-02-data-processing.R
@@ -65,12 +65,6 @@ test_that("does process_unknownsp work properly?", {
   expect_false("other" %in% rodents$species)
 })
 
-test_that("does remove_incomplete_censuses work properly?", {
-  rodents_with_incompletes <- clean_rodent_data(data_tables, incomplete = TRUE)
-  expect_gt(nrow(dplyr::filter(rodents_with_incompletes, plot == 24)),
-            nrow(dplyr::filter(rodents, plot == 24)))
-})
-
 test_that("does filter_plots work properly?", {
   rodents_longterm <- filter_plots(data_tables$trapping, length = "longterm")
   expect_equal(sort(unique(rodents_longterm$plot)), c(3, 4, 10, 11, 14, 15, 16, 17, 19, 21, 23))
--- tests/testthat/test-03-getRodentData.R
+++ tests/testthat/test-03-getRodentData.R
@@ -2,52 +2,43 @@ context("Check rodent data summaries")
 
 test_that("abundance returns expected results", {
   ab_notfilled <- abundance(path = ".", level = "Plot", type = "Rodents",
-                            length = "all", unknowns = FALSE, incomplete = FALSE,
+                            length = "all", unknowns = FALSE,
                             shape = "flat", time = "period", fillweight = FALSE,
                             na_drop = FALSE, zero_drop = FALSE, min_traps = 1,
-                            min_plots = NULL, effort = FALSE)
+                            min_plots = 24, effort = FALSE)
   test_ab <- dplyr::filter(ab_notfilled, period %in% 400:450)
-  expect_equal(nrow(test_ab), 23184)
+  expect_equal(nrow(test_ab), 25704)
   expect_true(sum(test_ab$abundance, na.rm = TRUE) == 10110)
   test_ab <- dplyr::filter(ab_notfilled, species == "DM", abundance > 0)
   expect_equal(max(test_ab$abundance, na.rm = TRUE), 17)
   expect_false(anyNA(test_ab))
 
   ab_filled <- abundance(path = ".", level = "Plot", type = "Rodents",
-                         length = "all", unknowns = FALSE, incomplete = FALSE,
+                         length = "all", unknowns = FALSE,
                          shape = "flat", time = "period", fillweight = TRUE,
                          na_drop = FALSE, zero_drop = FALSE, min_traps = 1,
-                         min_plots = NULL, effort = FALSE)
+                         min_plots = 24, effort = FALSE)
   expect_equal(ab_notfilled, ab_filled)
 })
 
 test_that("biomass returns expected results", {
 
   biom_filled <- biomass(path = ".", level = "Plot", type = "Rodents",
-                         length = "all", unknowns = FALSE, incomplete = FALSE,
+                         length = "all", unknowns = FALSE,
                          shape = "flat", time = "period", fillweight = TRUE,
                          na_drop = FALSE, zero_drop = FALSE, min_traps = 1,
-                         min_plots = NULL, effort = FALSE) %>%
+                         min_plots = 24, effort = FALSE) %>%
     dplyr::filter(period %in% 400:450)
 
   biom_notfilled <- biomass(path = ".", level = "Plot", type = "Rodents",
-                            length = "all", unknowns = FALSE, incomplete = FALSE,
+                            length = "all", unknowns = FALSE,
                             shape = "flat", time = "period", fillweight = FALSE,
                             na_drop = FALSE, zero_drop = FALSE, min_traps = 1,
-                            min_plots = NULL, effort = FALSE) %>%
+                            min_plots = 24, effort = FALSE) %>%
     dplyr::filter(period %in% 400:450)
 
-  biom_notfilled <- biomass(path = ".", level = "Plot", type = "Rodents",
-                            length = "all", unknowns = FALSE, incomplete = FALSE,
-                            shape = "flat", time = "period", fillweight = FALSE,
-                            na_drop = FALSE, zero_drop = FALSE, min_traps = 1,
-                            min_plots = NULL, effort = FALSE) %>%
-    dplyr::filter(period %in% 400:450)
-
-  expect_equal(nrow(biom_filled), 23184)
+  expect_equal(nrow(biom_filled), 25704)
   expect_equal(dim(biom_filled), dim(biom_notfilled))
-  expect_equal(sum(is.na(biom_filled$biomass)), 0)
-  expect_equal(sum(is.na(biom_notfilled$biomass)), 0)
   expect_equal(biom_filled$species, biom_notfilled$species)
   expect_equal(floor(dplyr::filter(biom_notfilled, period == 447, plot == 3,
                                    species == "BA")$biomass), 15)
@@ -58,23 +49,21 @@ test_that("biomass returns expected results", {
 test_that("energy returns expected results", {
 
   energy_filled <- energy(path = ".", level = "Plot", type = "Rodents",
-                          length = "all", unknowns = FALSE, incomplete = FALSE,
+                          length = "all", unknowns = FALSE,
                           shape = "flat", time = "period", fillweight = TRUE,
                           na_drop = FALSE, zero_drop = FALSE, min_traps = 1,
-                          min_plots = NULL, effort = FALSE) %>%
+                          min_plots = 24, effort = FALSE) %>%
     dplyr::filter(period %in% 400:450)
 
   energy_notfilled <- energy(path = ".", level = "Plot", type = "Rodents",
-                             length = "all", unknowns = FALSE, incomplete = FALSE,
+                             length = "all", unknowns = FALSE,
                              shape = "flat", time = "period", fillweight = FALSE,
                              na_drop = FALSE, zero_drop = FALSE, min_traps = 1,
-                             min_plots = NULL, effort = FALSE) %>%
+                             min_plots = 24, effort = FALSE) %>%
     dplyr::filter(period %in% 400:450)
 
-  expect_equal(nrow(energy_filled), 23184)
+  expect_equal(nrow(energy_filled), 25704)
   expect_equal(dim(energy_filled), dim(energy_notfilled))
-  expect_equal(sum(is.na(energy_filled$energy)), 0)
-  expect_equal(sum(is.na(energy_notfilled$energy)), 0)
   expect_equal(energy_filled$species, energy_notfilled$species)
   expect_equal(floor(dplyr::filter(energy_notfilled, period == 447, plot == 3,
                                    species == "BA")$energy), 9)
