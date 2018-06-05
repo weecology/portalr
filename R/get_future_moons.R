9aa76553693e32a120ad2e1ded99dbf904acdb67
Hao Ye <lhopitalified@gmail.com>
2018-06-05 14:38:39 -0400

    Formatting and tests for get_future_moons

--- R/get_future_moons.R
+++ R/get_future_moons.R
@@ -9,25 +9,25 @@
 #'
 #' @export
 #'
-get_future_moons <- function(moons, num_future_moons=12){
+get_future_moons <- function(moons, num_future_moons = 12) {
 
   #starting 6 days after latest new moon, to avoid overlap
-  most_recent_date = lubridate::as_date(tail(moons$newmoondate,1))+6
+  most_recent_date <- lubridate::as_date(tail(moons$newmoondate, 1)) + 6
 
-  dates=lubridate::as_date(most_recent_date:(most_recent_date+num_future_moons*31))
+  dates <- lubridate::as_date(most_recent_date:(most_recent_date + num_future_moons * 31))
 
-  futuremoondates = data.frame(newmoondate = dates, phase = lunar::lunar.phase(dates,name=8)) %>%
-    dplyr::filter(phase=="New") %>%
+  futuremoondates <- data.frame(newmoondate = dates, phase = lunar::lunar.phase(dates, name = 8)) %>%
+    dplyr::filter(phase == "New") %>%
     dplyr::mutate(group = cumsum(c(1, diff.Date(newmoondate)) > 5)) %>%
     dplyr::group_by(group) %>%
     dplyr::summarise(newmoondate = median(newmoondate))
 
-  newmoondates = futuremoondates$newmoondate[1:num_future_moons]
-  if(length(newmoondates)!=num_future_moons){
-    stop('Not enough new moons obtained. Expected ',num_future_moons,' got ',length(newmoondates))
+  newmoondates <- futuremoondates$newmoondate[seq(num_future_moons)]
+  if (length(newmoondates) != num_future_moons) {
+    stop('Not enough new moons obtained. Expected ', num_future_moons, ' got ', length(newmoondates))
   }
 
-  newmoons = data.frame(newmoonnumber = max(moons$newmoonnumber)+1:length(newmoondates),
+  newmoons <- data.frame(newmoonnumber = max(moons$newmoonnumber) + seq_along(newmoondates),
                         newmoondate = as.Date(newmoondates),
                         period = NA,
                         censusdate = as.Date(NA))
