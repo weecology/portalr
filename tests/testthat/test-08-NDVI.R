54ac545293373a25855e25939b95257b85e94dbb
DAPPERstats <dapperstats@gmail.com>
2018-07-07 20:55:08 -0700

    adding ndvi forecast tests

--- tests/testthat/test-08-NDVI.R
+++ tests/testthat/test-08-NDVI.R
@@ -31,3 +31,16 @@ test_that("Missing moon option data are filled", {
   ndvi_NA <- fill_missing_ndvi(ndvi_NA, "newmoon", lasttime)
   expect_that(any(is.na(ndvi_NA[, "ndvi"])), equals(FALSE))
 })
+
+test_that("Monthly NDVI can be forecast", {
+  fcast_vals <- fcast_ndvi(monthly_ndvi, "monthly", 1)
+  expect_that(class(fcast_vals[ , "ndvi"]), equals("numeric"))
+})
+
+test_that("Newmoon NDVI can be forecast", {
+  moons <- read.csv(
+             full_path('PortalData/Rodents/moon_dates.csv', portal_data_path),
+             na.strings = c(""), stringsAsFactors = FALSE)
+  fcast_vals <- fcast_ndvi(newmoon_ndvi, "newmoon", 1, moons)
+  expect_that(class(fcast_vals[ , "ndvi"]), equals("numeric"))
+})
