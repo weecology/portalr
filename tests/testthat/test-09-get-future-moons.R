f3f653187b3382b43c67468dbd2189746d6f1985
Hao Ye <lhopitalified@gmail.com>
2018-09-19 16:10:32 -0400

    move regression test into appropriate file

--- tests/testthat/test-09-get-future-moons.R
+++ tests/testthat/test-09-get-future-moons.R
@@ -13,14 +13,3 @@ test_that("get_future_moons returns expected results using current newmoons_tabl
   expect_equal(newmoons$period, rep(NA, 12))
   expect_equal(newmoons$censusdate, as.Date(rep(NA, 12)))
 })
-
-test_that("get_future_moons returns identical table using sample input", {
-  moons <- data.frame(newmoonnumber = c(1, 2),
-                      newmoondate = c("1977-07-16", "1977-08-14"),
-                      period = c(1, 2),
-                      censusdate = c("1977-07-16", "1977-08-19"))
-
-  newmoons <- get_future_moons(moons, num_future_moons = 10)
-  attributes(newmoons) <- attributes(newmoons)[sort(names(attributes(newmoons)))]
-  expect_identical(digest::digest(newmoons), "bec7d2ca15a8dad4d44775daf5239c53")
-})
