7c2345260e25e1202b753243e7b22c16f26a90ca
Hao Ye <lhopitalified@gmail.com>
2019-02-21 11:23:19 -0500

    add check function for default data path update tests

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,10 @@
 .onAttach <- function(libname, pkgname) {
-  if (interactive() && check_for_newer_data())
+  if (interactive() && check_default_data_path() && check_for_newer_data())
   {
     packageStartupMessage("The data in the default path `",
                           normalizePath(get_default_data_path()),
                           "` is either missing or out of date.\n",
                           "Consider updating it using `download_observations()`.")
   }
-
   invisible()
 }
