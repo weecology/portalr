c888daaf62a1ed06c2f28201790b4552717b9e74
Hao Ye <hye@ucsd.edu>
2019-07-05 16:18:23 -0400

    revert clean_data to original version

--- R/process_data_utils.R
+++ R/process_data_utils.R
@@ -11,8 +11,8 @@
 #' @noRd
 clean_data <- function(full_data, trapping_table, ...) {
   columns_to_keep <- colnames(full_data)
-  full_data <- unique(full_data) %>%
-    dplyr::left_join(unique(trapping_table), ...) %>%
+  full_data <- full_data %>%
+    dplyr::left_join(trapping_table, ...) %>%
     dplyr::filter(.data$qcflag == 1) %>%
     dplyr::select(columns_to_keep) %>%
     unique()
