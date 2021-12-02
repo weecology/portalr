a9369babcb91a2cfd4844b51170d3d312d1ff119
juniperlsimonis <dapperstats@gmail.com>
2021-12-02 12:21:38 -0800

    removing individual rodent stuff

--- R/summarize_individual_rodents.R
+++ R/summarize_individual_rodents.R
@@ -1,14 +1,9 @@
 #' @name summarize_individual_rodents
 #'
-#' @title Portal rodent individual-level data
+#' @title Return cleaned Portal rodent individual data
 #'
-#' @description Generic interface into creating summaries of the Portal 
-#'              rodent data at the individual level. It contains a number of 
-#'              arguments to specify the kind of data to summarize (at what 
-#'              level of aggregation) and various choices for dealing with 
-#'              data quality and output format. 
-#'              \cr \cr
-#'              \code{summarise_rodent_output} provides alternative spelling. 
+#' @description This function cleans and subsets the data based on a number
+#'   of arguments. It returns stake number and individual level data.
 #'
 #' @inheritParams summarize_rodent_data
 #'
@@ -16,65 +11,37 @@
 #'
 #' @export
 #'
-summarize_individual_rodents <- function (path = get_default_data_path(),
-                                          clean = TRUE, 
-                                          level = "Site",
-                                          type = "Rodents", 
-                                          length = "all", 
-                                          plots = length,
-                                          unknowns = FALSE, 
-                                          time = "period",
-                                          fillweight = FALSE, 
-                                          min_plots = 1, 
-                                          min_traps = 1,
-                                          download_if_missing = TRUE, 
-                                          quiet = FALSE) {
+summarize_individual_rodents <- function(path = get_default_data_path(),
+                                         clean = TRUE, type = "Rodents",
+                                         length = "all", unknowns = FALSE, time = "period",
+                                         fillweight = FALSE, min_plots = 1, min_traps = 1,
+                                         download_if_missing = TRUE, quiet = FALSE)
+{
 
-  if (!missing("length")) {
-  
-    warning("`length` is deprecated; use `plots` to specify subsets")
+  #### Get Data ----
+  data_tables <- load_rodent_data(path, download_if_missing = download_if_missing,
+                                  clean = clean, quiet = quiet)
 
-  }
+  #### Do initial cleaning ----
+  rodents <- clean_rodent_data(data_tables$rodent_data,
+                               data_tables$species_table,
+                               fillweight, type,
+                               unknowns)
 
-  data <- load_rodent_data(path = path, 
-                           download_if_missing = download_if_missing,
-                           clean = clean, quiet = quiet)
-
-  rodents <- clean_rodent_data(rodent_data = data$rodent_data, 
-                               species_table = data$species_table,
-                               fillweight = FALSE, type = type,
-                               unknowns = unknowns) %>%
-             clean_tags(clean = clean, quiet = quiet)
-
-
-  trapping <- filter_plots(data$trapping, plots = plots) %>%
-              join_plots(plots_table = data$plots_table)
-
-
-#
-#  working here
-#
-
-  join_trapping_to_rodents(rodents, trapping, data$trapping, min_plots, 
-                           min_traps) %>%
-  join_plots(data$plots_table) %>%
-  dplyr::select(c("id", "period", "month", "day" = "day.x", "year",
+  #### Filter by length and add treatment types ----
+  trapping <- filter_plots(data_tables$trapping, length)
+  rodents <- join_trapping_to_rodents(rodents, trapping, data_tables$trapping,
+                                      min_plots, min_traps) %>%
+    join_plots(data_tables$plots_table) %>%
+    dplyr::select(c("period", "month", "day" = "day.x", "year",
                     "treatment", "plot", "stake", "species",
-                    "sex", "reprod", "age", "testes", "vagina","pregnant",
-                    "nipples","lactation",
-                    "hfl", "wgt", "tag", "note2", "ltag", "note3")) %>%
-  add_time(rodents, data$newmoons_table, time)
-#
-# through here
-#
+                    "sex", "reprod", "age", "testes", "vagina","pregnant", "nipples","lactation",
+                    "hfl", "wgt", "tag", "note2", "ltag", "note3"))
 
+  #### use new moon number as time index if time == "newmoon" ----
+  return(add_time(rodents, data_tables$newmoons_table, time))
 }
 
-#' 
 #' @rdname summarize_individual_rodents
-#' 
 #' @export
-#' 
-summarise_individual_rodents <- function (...) { 
-  summarize_individual_rodents(...)
-}
+summarise_individual_rodents <- summarize_individual_rodents
